407d3c677511c36802f60ee8f5a95a499e58720a
Derek Hildreth <sendderek@gmail.com>
2025-09-02 17:17:44 -0600

    Commit to using the database
      - Fix static analysis issue in route.ts
      - Apply linter recommendations

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,20 +1,14 @@
-import { drizzle } from "drizzle-orm/postgres-js";
+import {drizzle} from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 const setup = () => {
   if (!process.env.DATABASE_URL) {
-    console.error("DATABASE_URL is not set");
-    return {
-      select: () => ({
-        from: () => [],
-      }),
-    };
+      throw new Error("DATABASE_URL is required");
   }
 
   // for query purposes
   const queryClient = postgres(process.env.DATABASE_URL);
-  const db = drizzle(queryClient);
-  return db;
+  return drizzle(queryClient);
 };
 
 export default setup();
